1bc4f4b34f5977cc8ad525a791fea7a277ff3aee
Andrey S <mailhouse@list.ru>
2024-02-15 22:57:13 +0500

    feat: deep diff

--- __tests__/src/parsers/json-parser.test.js
+++ __tests__/src/parsers/json-parser.test.js
@@ -3,7 +3,12 @@ import { test, expect } from '@jest/globals';
 import { cwd } from 'node:process';
 import * as path from 'path';
 
-import { getDiffData, getDiff } from '../../../src/parsers/json-parser.js';
+import {
+  getDiffData,
+  getDiff,
+  stringify,
+  getStringValue,
+} from '../../../src/parsers/json-parser.js';
 
 const filesData = [
   {
@@ -21,21 +26,134 @@ const filesData = [
     format: 'json',
     content: { timeout: 20, verbose: true, host: 'hexlet.io' },
   },
+  {
+    path: path.resolve(cwd(), './__fixtures__/file3.json'),
+    format: 'json',
+    content: {
+      common: {
+        setting1: 'Value 1',
+        setting2: 200,
+        setting3: true,
+        setting6: {
+          key: 'value',
+          doge: {
+            wow: '',
+          },
+        },
+      },
+      group1: {
+        baz: 'bas',
+        foo: 'bar',
+        nest: {
+          key: 'value',
+        },
+      },
+      group2: {
+        abc: 12345,
+        deep: {
+          id: 45,
+        },
+      },
+    },
+  },
+  {
+    path: path.resolve(cwd(), './__fixtures__/file4.json'),
+    format: 'json',
+    content: {
+      common: {
+        follow: false,
+        setting1: 'Value 1',
+        setting3: null,
+        setting4: 'blah blah',
+        setting5: {
+          key5: 'value5',
+        },
+        setting6: {
+          key: 'value',
+          ops: 'vops',
+          doge: {
+            wow: 'so much',
+          },
+        },
+      },
+      group1: {
+        foo: 'bar',
+        baz: 'bars',
+        nest: 'str',
+      },
+      group3: {
+        deep: {
+          id: {
+            number: 45,
+          },
+        },
+        fee: 100500,
+      },
+    },
+  },
 ];
+
+test('check json-parser stringify()', () => {
+  expect(getStringValue(123)).toEqual('123');
+});
+
+test('check json-parser stringify()', () => {
+  const obj = {
+    a: 5,
+    b: {
+      c: 'null',
+      d: [1, 2, 3],
+      e: { f: 10, g: 'value' },
+    },
+  };
+
+  expect(stringify(obj, '    ')).toEqual(`{
+    a: 5
+    b: {
+        c: null
+        d: [1,2,3]
+        e: {
+            f: 10
+            g: value
+        }
+    }
+}`);
+});
 
 test('check json-parser getDiffData()', () => {
   const data0 = getDiffData(filesData[0].content, filesData[1].content);
 
   expect(data0).toEqual([
-    { key: 'follow', status: 'deleted', values: { old: false } },
+    {
+      key: 'follow',
+      status: 'deleted',
+      values: { old: false },
+      children: [],
+    },
     {
       key: 'host',
       status: 'no-changes',
-      values: { old: 'hexlet.io', new: 'hexlet.io' },
+      values: { old: 'hexlet.io' },
+      children: [],
     },
-    { key: 'proxy', status: 'deleted', values: { old: '123.234.53.22' } },
-    { key: 'timeout', status: 'updated', values: { old: 50, new: 20 } },
-    { key: 'verbose', status: 'created', values: { new: true } },
+    {
+      key: 'proxy',
+      status: 'deleted',
+      values: { old: '123.234.53.22' },
+      children: [],
+    },
+    {
+      key: 'timeout',
+      status: 'updated',
+      values: { old: 50, new: 20 },
+      children: [],
+    },
+    {
+      key: 'verbose',
+      status: 'created',
+      values: { new: true },
+      children: [],
+    },
   ]);
 
   const data1 = getDiffData({}, {});
@@ -49,9 +167,20 @@ test('check json-parser getDiffData()', () => {
       key: 'host',
       status: 'created',
       values: { new: 'hexlet.io' },
+      children: [],
     },
-    { key: 'timeout', status: 'created', values: { new: 20 } },
-    { key: 'verbose', status: 'created', values: { new: true } },
+    {
+      key: 'timeout',
+      status: 'created',
+      values: { new: 20 },
+      children: [],
+    },
+    {
+      key: 'verbose',
+      status: 'created',
+      values: { new: true },
+      children: [],
+    },
   ]);
 
   const data3 = getDiffData(filesData[1].content, {});
@@ -61,9 +190,20 @@ test('check json-parser getDiffData()', () => {
       key: 'host',
       status: 'deleted',
       values: { old: 'hexlet.io' },
+      children: [],
     },
-    { key: 'timeout', status: 'deleted', values: { old: 20 } },
-    { key: 'verbose', status: 'deleted', values: { old: true } },
+    {
+      key: 'timeout',
+      status: 'deleted',
+      values: { old: 20 },
+      children: [],
+    },
+    {
+      key: 'verbose',
+      status: 'deleted',
+      values: { old: true },
+      children: [],
+    },
   ]);
 
   const data4 = getDiffData(filesData[1].content, filesData[1].content);
@@ -72,10 +212,21 @@ test('check json-parser getDiffData()', () => {
     {
       key: 'host',
       status: 'no-changes',
-      values: { old: 'hexlet.io', new: 'hexlet.io' },
+      values: { old: 'hexlet.io' },
+      children: [],
     },
-    { key: 'timeout', status: 'no-changes', values: { old: 20, new: 20 } },
-    { key: 'verbose', status: 'no-changes', values: { old: true, new: true } },
+    {
+      key: 'timeout',
+      status: 'no-changes',
+      values: { old: 20 },
+      children: [],
+    },
+    {
+      key: 'verbose',
+      status: 'no-changes',
+      values: { old: true },
+      children: [],
+    },
   ]);
 });
 
@@ -101,4 +252,55 @@ test('check json-parser getDiff()', () => {
   + timeout: 50
   - verbose: true
 }`);
+
+  const result3 = getDiff(filesData[2].content, filesData[3].content);
+
+  expect(result3).toEqual(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`);
+
+  const result4 = getDiff({}, {});
+
+  expect(result4).toEqual('{}');
 });
--- src/parsers/json-parser.js
+++ src/parsers/json-parser.js
@@ -11,9 +11,10 @@ const getDiffData = (json1, json2) => {
 
   sortedKeys.forEach((key) => {
     let status;
+    const children = [];
     const values = {};
 
-    // no changes
+    // deleted
     if (!Object.hasOwn(json2, key)) {
       status = 'deleted';
       values.old = json1[key];
@@ -22,10 +23,28 @@ const getDiffData = (json1, json2) => {
     else if (Object.hasOwn(json1, key)) {
       const isEqual = _.isEqual(json1[key], json2[key]);
 
-      status = isEqual ? 'no-changes' : 'updated';
+      // is equal
+      if (isEqual) {
+        status = 'no-changes';
+        values.old = json1[key];
+      }
+      // is not equal
+      else {
+        const isValue1Object = _.isPlainObject(json1[key]);
+        const isValue2Object = _.isPlainObject(json2[key]);
 
-      values.old = json1[key];
-      values.new = json2[key];
+        // Is object
+        if (isValue1Object && isValue2Object) {
+          status = 'no-changes';
+          children.push(...getDiffData(json1[key], json2[key]));
+        }
+        // Is not object
+        else {
+          status = 'updated';
+          values.old = json1[key];
+          values.new = json2[key];
+        }
+      }
     }
     // created
     else {
@@ -33,46 +52,121 @@ const getDiffData = (json1, json2) => {
       values.new = json2[key];
     }
 
-    result.push({ key, status, values });
+    result.push({
+      key,
+      status,
+      values,
+      children,
+    });
   });
 
   return result;
 };
 
-const getDiff = (json1, json2) => {
+const stringify = (obj, tab = '', startIndent = '') => {
+  const iter = (props, depth) => {
+    const keys = Object.keys(props);
+
+    if (!keys.length) return '';
+
+    const indent = tab.repeat(depth);
+
+    const values = keys.map((key) => {
+      const keyPart = `${startIndent}${indent}${tab}${key}: `;
+
+      if (_.isPlainObject(props[key])) {
+        return keyPart + iter(props[key], depth + 1);
+      }
+
+      if (_.isArray(props[key])) {
+        return keyPart + JSON.stringify(props[key]);
+      }
+
+      return keyPart + props[key];
+    });
+
+    return `{\n${values.join('\n')}\n${startIndent}${indent}}`;
+  };
+
+  return iter(obj, 0);
+};
+
+const getStringValue = (value, tab = '', startIndent = '') => {
+  if (_.isPlainObject(value)) {
+    return stringify(value, tab, startIndent);
+  }
+
+  if (_.isArray(value)) {
+    return JSON.stringify(value);
+  }
+
+  return value;
+};
+
+const getDiff = (json1, json2, indentTemplate = '  $ ') => {
   const data = getDiffData(json1, json2);
 
-  const prefixes = { space: '    ', minus: '  - ', plus: '  + ' };
+  if (!data.length) return '{}';
 
-  const result = data.reduce((acc, { key, status, values }) => {
-    switch (status) {
-      case 'no-changes': {
-        acc.push(`${prefixes.space}${key}: ${values.old}`);
-        break;
-      }
-      case 'created': {
-        acc.push(`${prefixes.plus}${key}: ${values.new}`);
-        break;
-      }
-      case 'updated': {
-        acc.push(`${prefixes.minus}${key}: ${values.old}`);
-        acc.push(`${prefixes.plus}${key}: ${values.new}`);
-        break;
-      }
-      case 'deleted': {
-        acc.push(`${prefixes.minus}${key}: ${values.old}`);
-        break;
-      }
-      default: {
-        break;
-      }
-    }
+  const [space, minus, plus] = [' ', '-', '+'].map((symbol) => {
+    const indent = indentTemplate.replace(/\$/g, symbol);
+    return indent;
+  });
 
-    return acc;
-  }, []);
+  const iter = (iterData, depth) => {
+    const indent = space.repeat(depth);
 
-  return `{\n${result.join('\n')}\n}`;
+    // eslint-disable-next-line
+    const result = iterData.reduce((acc, { key, status, values, children }) => {
+      const formattedNewValue = getStringValue(
+        values.new,
+        space,
+        indent + space,
+      );
+      const formattedOldValue = getStringValue(
+        values.old,
+        space,
+        indent + space,
+      );
+
+      switch (status) {
+        case 'no-changes': {
+          // Has changed children
+          if (children.length) {
+            acc.push(`${indent}${space}${key}: ${iter(children, depth + 1)}`);
+          }
+          // No changed children
+          else {
+            acc.push(`${indent}${space}${key}: ${formattedOldValue}`);
+          }
+          break;
+        }
+        case 'created': {
+          acc.push(`${indent}${plus}${key}: ${formattedNewValue}`);
+          break;
+        }
+        case 'updated': {
+          acc.push(`${indent}${minus}${key}: ${formattedOldValue}`);
+          acc.push(`${indent}${plus}${key}: ${formattedNewValue}`);
+          break;
+        }
+        case 'deleted': {
+          acc.push(`${indent}${minus}${key}: ${formattedOldValue}`);
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+
+      return acc;
+    }, []);
+
+    return `{\n${result.join('\n')}\n${indent}}`;
+  };
+
+  return iter(data, 0);
 };
 
 // eslint-disable-next-line
-export { getDiffData, getDiff };
+export { getDiffData, getDiff, stringify };
