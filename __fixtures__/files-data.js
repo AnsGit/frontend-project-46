d317d448b2a3368d81f85155fece80ff9a026664
Andrey S <mailhouse@list.ru>
2024-02-16 20:16:16 +0500

    fix: refactor tests

---
+++ __fixtures__/files-data.js
@@ -0,0 +1,70 @@
+import { cwd } from 'node:process';
+import * as path from 'path';
+
+export default [
+  {
+    path: path.resolve(cwd(), './file3.json'),
+    format: 'json',
+    content: {
+      common: {
+        setting1: 'Value 1',
+        setting2: 200,
+        setting3: true,
+        setting6: {
+          key: 'value',
+          doge: {
+            wow: '',
+          },
+        },
+      },
+      group1: {
+        baz: 'bas',
+        foo: 'bar',
+        nest: {
+          key: 'value',
+        },
+      },
+      group2: {
+        abc: 12345,
+        deep: {
+          id: 45,
+        },
+      },
+    },
+  },
+  {
+    path: path.resolve(cwd(), './file4.json'),
+    format: 'json',
+    content: {
+      common: {
+        follow: false,
+        setting1: 'Value 1',
+        setting3: null,
+        setting4: 'blah blah',
+        setting5: {
+          key5: 'value5',
+        },
+        setting6: {
+          key: 'value',
+          ops: 'vops',
+          doge: {
+            wow: 'so much',
+          },
+        },
+      },
+      group1: {
+        foo: 'bar',
+        baz: 'bars',
+        nest: 'str',
+      },
+      group3: {
+        deep: {
+          id: {
+            number: 45,
+          },
+        },
+        fee: 100500,
+      },
+    },
+  },
+];
--- __tests__/src/formatters/diff-plain-formatter.test.js
+++ __tests__/src/formatters/diff-plain-formatter.test.js
@@ -1,8 +1,5 @@
 import { test, expect } from '@jest/globals';
 
-import { cwd } from 'node:process';
-import * as path from 'path';
-
 import { getDiffData } from '../../../src/parsers/json-comparer.js';
 
 import {
@@ -10,73 +7,7 @@ import {
   getPlainDiff,
 } from '../../../src/formatters/diff-plain-formatter.js';
 
-const filesData = [
-  {
-    path: path.resolve(cwd(), './__fixtures__/file3.json'),
-    format: 'json',
-    content: {
-      common: {
-        setting1: 'Value 1',
-        setting2: 200,
-        setting3: true,
-        setting6: {
-          key: 'value',
-          doge: {
-            wow: '',
-          },
-        },
-      },
-      group1: {
-        baz: 'bas',
-        foo: 'bar',
-        nest: {
-          key: 'value',
-        },
-      },
-      group2: {
-        abc: 12345,
-        deep: {
-          id: 45,
-        },
-      },
-    },
-  },
-  {
-    path: path.resolve(cwd(), './__fixtures__/file4.json'),
-    format: 'json',
-    content: {
-      common: {
-        follow: false,
-        setting1: 'Value 1',
-        setting3: null,
-        setting4: 'blah blah',
-        setting5: {
-          key5: 'value5',
-        },
-        setting6: {
-          key: 'value',
-          ops: 'vops',
-          doge: {
-            wow: 'so much',
-          },
-        },
-      },
-      group1: {
-        foo: 'bar',
-        baz: 'bars',
-        nest: 'str',
-      },
-      group3: {
-        deep: {
-          id: {
-            number: 45,
-          },
-        },
-        fee: 100500,
-      },
-    },
-  },
-];
+import filesData from '../../../__fixtures__/files-data.js';
 
 test('check json-parser getPlainText()', () => {
   const text0 = getPlainText('deleted', 'common.setting2');
